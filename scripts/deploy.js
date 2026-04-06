0d4b771fcdcc67d65dee94ae2b93198fc77b4ac9
wenboc1 <wenbochen2002@outlook.com>
2026-04-05 18:35:21 -0700

    align with A2

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -2,22 +2,19 @@ const hre = require("hardhat");
 
 async function main() {
   const [deployer] = await hre.ethers.getSigners();
-
   console.log("Deploying contracts with account:", deployer.address);
-
-  const CampusGov = await hre.ethers.getContractFactory("CampusGov");
-  const campusGov = await CampusGov.deploy();
-  await campusGov.waitForDeployment();
-
-  console.log("CampusGov deployed to:", await campusGov.getAddress());
 
   const CGOVToken = await hre.ethers.getContractFactory("CGOVToken");
   const initialSupply = hre.ethers.parseUnits("1000000", 18);
-
   const cgovToken = await CGOVToken.deploy(initialSupply);
   await cgovToken.waitForDeployment();
+  const tokenAddress = await cgovToken.getAddress();
+  console.log("CGOVToken deployed to:", tokenAddress);
 
-  console.log("CGOVToken deployed to:", await cgovToken.getAddress());
+  const CampusGov = await hre.ethers.getContractFactory("CampusGov");
+  const campusGov = await CampusGov.deploy(tokenAddress);
+  await campusGov.waitForDeployment();
+  console.log("CampusGov deployed to:", await campusGov.getAddress());
 }
 
 main().catch((error) => {
