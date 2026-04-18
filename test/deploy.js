b3fd20524f29ed8253e5a3f4a4198966bbd93eb1
Wangchen Xu <xuwc2021@student.ubc.ca>
2026-04-17 21:15:23 -0700

    Finalized

--- test/deploy.js
+++ test/deploy.js
@@ -5,15 +5,16 @@ async function main() {
   console.log("Deploying contracts with account:", deployer.address);
 
   const CGOVToken = await hre.ethers.getContractFactory("CGOVToken");
-  const initialSupply = hre.ethers.parseUnits("1000000", 18);
-  const cgovToken = await CGOVToken.deploy(initialSupply);
+  const cgovToken = await CGOVToken.deploy();
   await cgovToken.waitForDeployment();
+
   const tokenAddress = await cgovToken.getAddress();
   console.log("CGOVToken deployed to:", tokenAddress);
 
   const CampusGov = await hre.ethers.getContractFactory("CampusGov");
   const campusGov = await CampusGov.deploy(tokenAddress);
   await campusGov.waitForDeployment();
+
   console.log("CampusGov deployed to:", await campusGov.getAddress());
 }
 
